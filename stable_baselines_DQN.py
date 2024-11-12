f9e8d96422b961a1105fb1226efa4da80248bee9
Sourav1429 <68081019+Sourav1429@users.noreply.github.com>
2024-11-12 15:43:46 -0500

    Add files via upload

---
+++ stable_baselines_DQN.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 12 15:31:57 2024
+
+@author: gangu
+"""
+
+import gym
+
+from stable_baselines.common.vec_env import DummyVecEnv
+from stable_baselines.deepq.policies import MlpPolicy
+from stable_baselines import DQN
+
+env = gym.make('CartPole-v1')
+
+model = DQN(MlpPolicy, env, verbose=1)
+model.learn(total_timesteps=25000)
+model.save("deepq_cartpole")
+
+del model # remove to demonstrate saving and loading
+
+model = DQN.load("deepq_cartpole")
+
+obs = env.reset()
+while True:
+    action, _states = model.predict(obs)
+    obs, rewards, dones, info = env.step(action)
+    env.render()
